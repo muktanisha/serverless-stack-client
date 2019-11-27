369d427bc25bee27e782b5e238500ce8955affad
muktanisha <nisha.mukta@srijan.net>
2019-11-27 15:42:58 +0530

    Setting up our React app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import Routes from "./Routes";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Scratch - A simple note taking app</h1>
-        </div>
-      </div>
+      <div className="App container">
+            <Navbar fluid collapseOnSelect>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <Link to="/">Scratch</Link>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+              </Navbar.Header>
+              <Navbar.Collapse>
+                <Nav pullRight>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Routes />
+          </div>
     );
   }
 }
